ec7098f5d5f465019b722d0555d4b1b0ef952535
Wojciech Kupaj <wkupaj@gmail.com>
2022-06-12 20:30:17 +0200

    evaluation

--- eval.py
+++ eval.py
@@ -1,20 +1,27 @@
 from __future__ import absolute_import, division, print_function
 
-import logging
 import os
 import random
-import sys
 
 import numpy as np
 import torch
+from sklearn.metrics import classification_report
 
-from core.data_utils import AeProcessor, create_dataset, convert_examples_to_features
+from pytorch_transformers import AdamW, WarmupLinearSchedule
+from core.data_utils import AeProcessor, create_dataset, convert_examples_to_features, AscProcessor
 from core.model import XLMRMultiLabelClassification
-from core.train_utils import evaluate_model
 
 
 def eval():
-    data_dir = 'data/aspect_on_review/extracted/'
+    data_dir = 'data/aspect_on_sentence/extracted/'
+    dropout = 0.2
+    eval_batch_size = 32
+    max_seq_length = 128
+    model_dir = 'models/ae_absa/'
+    pretrained_path = 'models/roberta_base_fairseq'
+    seed = 42
+
+    data_dir = 'data/aspect_on_sentence/extracted/'
     dropout = 0.2
     adam_epsilon = 1e-08
     eval_batch_size = 32
@@ -22,63 +29,83 @@ def eval():
     learning_rate = 6e-05
     max_grad_norm = 1.0
     max_seq_length = 128
-    num_train_epochs = 50
-    output_dir = 'models/absa/'
+    num_train_epochs = 3
     pretrained_path = 'models/roberta_large_fairseq'
     seed = 42
-    train_batch_size = 8
+    train_batch_size = 32
     warmup_proportion = 0.0
     weight_decay = 0.01
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                        datefmt='%m/%d/%Y %H:%M:%S',
-                        level=logging.INFO,
-                        filename=os.path.join(output_dir, "log.txt"))
-    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-    logger = logging.getLogger(__name__)
+    train_batch_size = train_batch_size // gradient_accumulation_steps
 
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
 
-    processor = AeProcessor()
+    mode = 'ae'
+    if mode == 'asc':
+        processor = AscProcessor()
+    elif mode == 'ae':
+        processor = AeProcessor()
+
     label_list = processor.get_labels(data_dir)
     print(*label_list, sep="\n")
 
     num_labels = len(label_list)
-
+    train_examples = processor.get_train_examples(data_dir)
+    num_train_optimization_steps = int(
+        len(train_examples) / train_batch_size / gradient_accumulation_steps) * num_train_epochs
 
     hidden_size = 768 if 'base' in pretrained_path else 1024
 
-    # device = 'cuda' if (torch.cuda.is_available() and not args.no_cuda) else 'cpu'
-    device = "cpu"
-    logger.info(device)
+    device = "cuda:0"
+    print(device)
 
     model = XLMRMultiLabelClassification(pretrained_path=pretrained_path,
                                          n_labels=num_labels, hidden_size=hidden_size,
-                                         dropout_p=dropout, device=device)
+                                         dropout_p=dropout, device=device, mode=mode)
 
     model.to(device)
+    no_decay = ['bias', 'final_layer_norm.weight']
+
+    params = list(model.named_parameters())
+
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in params if not any(
+            nd in n for nd in no_decay)], 'weight_decay': weight_decay},
+        {'params': [p for n, p in params if any(
+            nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+
+    warmup_steps = int(warmup_proportion * num_train_optimization_steps)
+    optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate, eps=adam_epsilon)
+    scheduler = WarmupLinearSchedule(optimizer, warmup_steps=warmup_steps, t_total=num_train_optimization_steps)
+    model.to(device)
+
+    state_dict = torch.load(open(os.path.join(model_dir, 'model.pt'), 'rb'), map_location=torch.device('cpu'))
+    model.load_state_dict(state_dict)
+    print("Loaded saved model")
 
     eval_examples = processor.get_test_examples(data_dir)
     eval_features = convert_examples_to_features(eval_examples, max_seq_length, model.encode_word)
 
-    logger.info("***** Running evaluation *****")
-    logger.info("  Num examples = %d", len(eval_examples))
-    logger.info("  Batch size = %d", eval_batch_size)
+    print("***** Running evaluation *****")
+    print("  Num examples = %d", len(eval_examples))
+    print("  Batch size = %d", eval_batch_size)
 
     eval_data = create_dataset(eval_features)
-    f1_score, report = evaluate_model(model, eval_data, eval_batch_size, device)
+    y_pred_list = []
+    y_test = []
+    with torch.no_grad():
+        model.eval()
+        for X_eval, y_eval in eval_data:
+            X_eval, y_eval = X_eval.to(device), y_eval.to(device)
 
-    logger.info("\n%s", report)
-    output_eval_file = os.path.join(output_dir, "eval_results.txt")
-    with open(output_eval_file, "w") as writer:
-        logger.info("***** Writing results to file *****")
-        writer.write(report)
-        logger.info("Done.")
+            y_eval_pred = model(X_eval)
+            y_test.append(y_eval.cpu().numpy())
+            y_pred_list.append(y_eval_pred.cpu().numpy())
+
+    print(classification_report(y_test, y_pred_list))
 
 
 if __name__ == "__main__":
